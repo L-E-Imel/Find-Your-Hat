3c494e740ac028cf1ef8737a89c9b5a34ff9af6b
L-E-Imel <93445437+L-E-Imel@users.noreply.github.com>
2022-02-13 14:48:51 -0500

    beginnings of the game. Taking a break to learn more about Node.

--- main.js
+++ main.js
@@ -4,3 +4,35 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+
+class Field {
+    constructor(field) {
+        this.field = field;
+    }
+    print(field) {
+        console.log(field.join(''));
+    }
+    static generateField(h, w) {
+        let fieldArray = [];
+        fieldArray.length = h;
+        let randNum = Math.floor(Math.random() * 3);
+        const squares = [hat, hole, fieldCharacter];
+        for (let i = 0; i < fieldArray.length; i++) {
+            let tempArr = [];
+            tempArr.length = w;
+            for (let j = 0; j < tempArr.length; j++) {
+                tempArr.push(squares[randNum]);
+                console.log(tempArr);
+            }
+            fieldArray.push(tempArr);
+            console.log(fieldArray);
+        }
+        return fieldArray;
+        
+    }
+}
+
+const play = Field.generateField(5, 7);
+
+
